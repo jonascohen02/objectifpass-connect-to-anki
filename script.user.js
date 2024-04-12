d827a02f7b52ef831ac50b81e3f27e23199cb25a
jonascohen02 <83279036+jonascohen02@users.noreply.github.com>
2024-04-12 17:38:30 +0200

    Add files via upload

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Objectif Pass Anki To Connect
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.5
 // @updateURL    https://github.com/jonascohen02/objectifpass-connect-to-anki/raw/main/script.user.js
 // @downloadURL  https://github.com/jonascohen02/objectifpass-connect-to-anki/raw/main/script.user.js
 // @description  Adding buttons on OP to redirect to Anki and other cool tools
@@ -13,93 +13,51 @@
 
 (function() {
     'use strict';
-    //Personaliser qcm affiche pour ne pas afficher directement les réponses
-    if (window.location.pathname.includes('qcm/affiche')) {
-        var toggle = 0,
-            innerOriginal;
-        window.addEventListener("load", function() {
-            innerOriginal = structuredClone(document.querySelector('.QCM_block').innerHTML);
-            toggleSerieCorrectionAlwreadyDone(true);
-            $('.QCM_question').css('cursor', 'pointer').on('click', toggleSerieCorrectionAlwreadyDone);
-        });
-
-        function toggleSerieCorrectionAlwreadyDone(firstTime) {
-            var elementToOuter = document.querySelectorAll('.QCM_reponse');
-            var button = '<a class="QCM_reponse switch icons"><span class="ss-on" style="display: none;"></span><span class="ss-slider" style="left: 0px;"></span></a>';
-            if (firstTime) {
-                elementToOuter.forEach(function(e) {
-                    e.classList.add('answerItem')
-                });
-                elementToOuter.forEach(function(e) {
-                    e.classList.toggle('notDisplay')
-                });
-                elementToOuter.forEach(function(e) {
-                    e.insertAdjacentHTML('beforebegin', '<a class="QCM_reponse switch icons ss_alreadyOn"><span class="ss-on" style="display: none;"></span><span class="ss-slider" style="left: 0px;"></span></a>');
-                });
-                toogleExplicationOnLoad()
-            } else {
-                toogleExplication()
-            };
-            if (!toggle) {
-
-            } else {
-                document.querySelector('.QCM_block').innerHTML = innerOriginal;
-                document.querySelectorAll('.QCM_ligne_explication .bouton_bleu').forEach(function(element) {
-                    element.remove();
-                });
-                addButton();
-                $('.QCM_question').css('cursor', 'pointer').on('click', toggleSerieCorrectionAlwreadyDone)
-            }
-            toggle = toggle == 0 ? 1 : 0;
-        }
-    }
-
-
 
     //AJOUTER LES BOUTONS ET TOUTES LES FONCTIONS DE RECUPERATION DE DONNEES
-
+    //A EXECUTER SUR TOUS LES SITES OBJECTIF PASS
     let styleTag = document.createElement("style");
     styleTag.innerHTML = ".centerClass { text-align: center; } .notDisplay{ display:none ! important;}";
     document.head.appendChild(styleTag);
     var qcms = {};
-    var timeouts = [],
-        sync1 = 0,
-        sync2 = 0,
-        qcmNumber,
-        ue,
-        enonce,
-        annaleTag;
-
+    var timeouts = {
+        running: [],
+        cleared: []},
+    sync1 = 0,
+    sync2 = 0,
+    qcmNumber,
+    ue,
+    enonce,
+    annaleTag;
+    //VARIABLES SYNC PERMETTENT DE VOIR SI LE CLICK SUR LE QCMBLOCK QUI TOGGLE LA CORRECTION COINCIDE AVEC UN CLICK SUR UN BOUTON CONTENU A L'INTERIEUR ET DONC ADDTOANKI AUQUEL CAS IL FAUT EMPECHER LE TOGGLE DE FAIRE CELA CAR JE VEUX CONSERVER LE CLICK SUR TOUT LE QCM BLOCK
     const UeDefault = 12,
-          mobile = navigator.userAgentData ? navigator.userAgentData.mobile : true,
-          maxIntervalClick = 900;
+    mobile = navigator.userAgentData ? navigator.userAgentData.mobile: true,
+    maxIntervalClick = 200;
     //var correctionSerie = $._data($('.serie :submit')[0], 'events').click[0].handler;
 
     window.addToAnki = function(e, i) {
         sync1++;
-        //PERMET DE
-        if (timeouts) {
-            e.nbClick = timeouts.length;
-            console.log(e.nbClick);
-            timeouts.forEach(function(timeout) {
-                clearTimeout(timeout)
-            });
-            timeouts = [];
-        }
+        //ON DESYNCHRONISE sync1 et sync2  ON REAJUSTERA sync2 A LA FIN DU TOOGLE
+
+        //COMPTE LE NOMBRE DE TIMEOUTS CLEARED ENTRE PLUSIEURS CLICK SI L'INTERVALLE ENTRE DEUX CLICK EST TOUJOURS + PETIT QIE 200ms (maxIntervalClick)
+        e.nbClick = timeouts.cleared.length + 1;
+        console.log(e.nbClick);
+        timeouts.cleared = [];
+        timeouts.running = [];
         var qcm = qcms[i];
         var recto = qcm.qst,
-            verso = qcm.answ,
-            simulateMobile = mobile,
-            dataToAdd = {
-                "action": "multi",
-                "params": {
-                    "actions": []
+        verso = qcm.answ,
+        simulateMobile = mobile,
+        dataToAdd = {
+            "action": "multi",
+            "params": {
+                "actions": []
             }
         }
         if (e.altKey) {
             simulateMobile = true;
         }
-        if (e.ctrlKey | e.nbClick == 3) {
+        if (e.ctrlKey | e.nbClick >= 3) {
             recto = "<ul>";
             verso = "<ul>";
             for (var key in qcms) {
@@ -122,16 +80,16 @@
             recto += "</ul>";
             verso += "</ul>";
         }
-        if ((e.shiftKey && e.ctrlKey) | e.nbClick == 3) {
+        if ((e.shiftKey && e.ctrlKey) | e.nbClick >= 3) {
             recto = enonce.replace(/\(Annales ([0-9]{4})\/([0-9]{4})\)/g, '') + " " + recto;
         } else if (e.shiftKey | e.nbClick == 2) {
             recto = enonce.replace(/\(Annales ([0-9]{4})\/([0-9]{4})\)/g, '') + " <br>" + recto;
         };
         var dataEnonceImg = "",
-            dataCorrectionImg = "";
+        dataCorrectionImg = "";
         if (!simulateMobile) {
             var enonceImg = document.querySelector('img.enonce'),
-                correctionImg = document.querySelector('img.correction');
+            correctionImg = document.querySelector('img.correction');
 
             if (enonceImg) {
                 var enonceSrc = enonceImg.src;
@@ -147,19 +105,19 @@
                 dataToAdd.params.actions.push(dataEnonceImg);
                 dataEnonceImg = "<br><img  src=\"" + fileNameEnonce + "\">";
             }
-        }
-        if (correctionImg) {
-            var correctionSrc = correctionImg.src
-            var fileNameCorrection = correctionSrc.replace('https://www.objectifpass.fr/qcm_images/', '');
-            dataCorrectionImg = {
-                "action": "storeMediaFile",
-                "params": {
-                    "filename": fileNameCorrection,
-                    "url": correctionSrc
+            if (correctionImg) {
+                var correctionSrc = correctionImg.src
+                var fileNameCorrection = correctionSrc.replace('https://www.objectifpass.fr/qcm_images/', '');
+                dataCorrectionImg = {
+                    "action": "storeMediaFile",
+                    "params": {
+                        "filename": fileNameCorrection,
+                        "url": correctionSrc
+                    }
                 }
+                dataToAdd.params.actions.push(dataCorrectionImg);
+                dataCorrectionImg = "<img  src=\"" + fileNameCorrection + "\"><br>";
             }
-            dataToAdd.params.actions.push(dataCorrectionImg);
-            dataCorrectionImg = "<img  src=\"" + fileNameCorrection + "\"><br>";
         }
         recto = recto + dataEnonceImg;
         verso = dataCorrectionImg + verso;
@@ -202,33 +160,33 @@
         var chaineRegex = "\\(Annales ([0-9]{4})\/([0-9]{4})\\)";
         var regex = new RegExp(chaineRegex);
         var regexResult = regex.exec(enonce);
-        annaleTag = regexResult == undefined ? "" : "y_annale_" + regexResult[1] + "-" + regexResult[2];
+        annaleTag = regexResult == undefined ? "": "y_annale_" + regexResult[1] + "-" + regexResult[2];
         return annaleTag;
     }
 
     function getQcmNumber() {
         var openFormElement = document.querySelector('#threadOpenForm div.text')
-        var openForm = openFormElement == undefined ? "" : openFormElement.innerText;
+        var openForm = openFormElement == undefined ? "": openFormElement.innerText;
         var chaineRegex = "Poser une question sur le QCM ([0-9]*) \\?";
         var regex = new RegExp(chaineRegex);
         var regexResult = regex.exec(openForm);
-        qcmNumber = regexResult == undefined ? "9999" : regexResult[1];
+        qcmNumber = regexResult == undefined ? "9999": regexResult[1];
         return qcmNumber;
     }
 
     function getUe() {
         var completeUeElement = document.querySelector('#sidebar p');
-        var completeUe = completeUeElement == undefined ? "" : completeUeElement.innerText;
+        var completeUe = completeUeElement == undefined ? "": completeUeElement.innerText;
         var chaineRegex = "UE( spé )?([0-9]+)";
         var regex = new RegExp(chaineRegex);
         var regexResult = regex.exec(completeUe);
-        ue = regexResult == undefined ? UeDefault : regexResult[2];
+        ue = regexResult == undefined ? UeDefault: regexResult[2];
         return ue;
     }
 
     function getEnonce() {
         var completeEnonceElement = document.querySelector('.QCM_enonce');
-        enonce = completeEnonceElement == undefined ? "Enonce Nulle" : completeEnonceElement.innerText.trim();
+        enonce = completeEnonceElement == undefined ? "Enonce Nulle": completeEnonceElement.innerText.trim();
         return enonce + "<br>";
     }
 
@@ -251,8 +209,10 @@
             };
             if (mobile) {
                 var buttonToCatch = document.createElement('span');
+                var eventToCatch = "touchend";
             } else {
                 var buttonToCatch = document.createElement('button');
+                var eventToCatch = "click";
             }
             buttonToCatch.id = 'addToAnki_' + i;
             buttonToCatch.className = 'bouton_bleu';
@@ -260,32 +220,47 @@
             buttonToCatch.innerText = 'Ajouter à Anki';
             answer.appendChild(buttonToCatch);
             answer.classList.add("centerClass");
-            buttonToCatch.addEventListener('click', function(e) {
+            buttonToCatch.addEventListener(eventToCatch, function(e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
+                if (timeouts.running) {
+                    timeouts.running.forEach(function(timeout) {
+                        clearTimeout(timeout);
+                        timeouts.cleared.push(timeout);
+                    });
+                    timeouts.running = [];
+                }
                 let id = e.srcElement.id.replace('addToAnki_', '');
-                timeouts.push(setTimeout(function() {
+                timeouts.running.push(setTimeout(function() {
                     addToAnki(e, id)
                 }, maxIntervalClick));
             });
             i++;
         })
         try {
-            $._data($('.serie .QCM_block').css('cursor', 'pointer')[0], 'events').click[0].handler = toogleCorrection
+            $._data($('.serie .QCM_block').css('cursor',
+                'pointer')[0],
+                'events').click[0].handler = toogleCorrection
         } catch {
             return false
         };
     }
     window.addButton = addButton;
     //$('.serie :submit').off('click', correctionSerie);
-    addButton();
+    //addButton();
     $('.serie :submit').on('click', addButton);
-    $(document).on('ajaxPageLoad', function(event) {
-        $('.serie :submit').on('click', addButton);
-    });
+    $(document).on('ajaxPageLoad',
+        function(event) {
+            $('.serie :submit').on('click',
+                addButton);
+        }
+    );
 
-    function toogleCorrection(element = this, isAll = false, toggleTime = 150) {
+    function toogleCorrection(element = this,
+        isAll = false,
+        toggleTime = 150) {
         element = $('.QCM_block');
+        //PAS DE CLICK SUR LE BOUTON ADDTOANI
         if (sync1 == sync2) {
             var $explication = $(element).find('.QCM_ligne_explication');
             window.element = element;
@@ -338,9 +313,49 @@
                 }
             });
 
-            xhr.open('POST', 'http://127.0.0.1:8765');
+            xhr.open('POST',
+                'http://127.0.0.1:8765');
             xhr.send(JSON.stringify(data));
         });
+    }
+
+    //Personaliser qcm affiche pour ne pas afficher directement les réponses
+    if (window.location.pathname.includes('qcm/affiche')) {
+        var toggle = 0,
+        innerOriginal;
+        window.addEventListener("load", function() {
+            innerOriginal = structuredClone(document.querySelector('.QCM_block').innerHTML);
+            toggleSerieCorrectionAlwreadyDone(true);
+            $('.QCM_question').css('cursor', 'pointer').on('click', toggleSerieCorrectionAlwreadyDone);
+        });
+
+        function toggleSerieCorrectionAlwreadyDone(firstTime) {
+            var elementToOuter = document.querySelectorAll('.QCM_reponse');
+            var button = '<a class="QCM_reponse switch icons"><span class="ss-on" style="display: none;"></span><span class="ss-slider" style="left: 0px;"></span></a>';
+            if (firstTime) {
+                elementToOuter.forEach(function(e) {
+                    e.classList.add('answerItem')
+                });
+                elementToOuter.forEach(function(e) {
+                    e.classList.toggle('notDisplay')
+                });
+                elementToOuter.forEach(function(e) {
+                    e.insertAdjacentHTML('beforebegin', '<a class="QCM_reponse switch icons ss_alreadyOn"><span class="ss-on" style="display: none;"></span><span class="ss-slider" style="left: 0px;"></span></a>');
+                });
+                toogleExplicationOnLoad()
+            } else {
+                toogleExplication()
+            };
+            if (!toggle) {} else {
+                document.querySelector('.QCM_block').innerHTML = innerOriginal;
+                document.querySelectorAll('.QCM_ligne_explication .bouton_bleu').forEach(function(element) {
+                    element.remove();
+                });
+                addButton();
+                $('.QCM_question').css('cursor', 'pointer').on('click', toggleSerieCorrectionAlwreadyDone)
+            }
+            toggle = toggle == 0 ? 1: 0;
+        }
     }
 
 })();
